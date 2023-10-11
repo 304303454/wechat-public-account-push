86f270a9af487e709191d8c4732aa0bb88653fc6
304303454 <112739973+304303454@users.noreply.github.com>
2023-10-11 16:05:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,7 +57,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '吕玟', year: '2005', date: '03-11',
+          type: '生日', name: '吕口口', year: '2005', date: '03-11',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '05-01',
